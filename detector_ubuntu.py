c9e5ea4488ae0fbbbd9eb81923862eba7b290d6d
Anvar Alisheri <anvaralisheri@Anvars-MacBook-Pro.local>
2020-11-23 14:18:38 +0900

    Adding detector for macs

--- detector_ubuntu.py
+++ detector_ubuntu.py
@@ -10,7 +10,7 @@ import sys
 Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
 filename = askopenfilename() # show an "Open" dialog box and return the path to the selected file
 
-os.system('./darknet detector test model_single_class/mymodel.data model_single_class/yolov3-tiny.cfg backup/yolov3-tiny_15000.weights -dont_show ' + filename)
+os.system('./darknet_ubuntu detector test model_single_class/mymodel.data model_single_class/yolov3-tiny.cfg backup/yolov3-tiny_15000.weights -dont_show ' + filename)
 
 predicted_image = Image.open("predictions.jpg")
 predicted_image.show()
